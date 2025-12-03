11e9f18badfedf9266aa50164767928277e1bbe8
Rinaldi Rizqi Fauzi <rinaldirizqifauzi@gmail.com>
2025-12-03 13:40:58 +0700

    update

--- app/Scopes/LeaveDaysQueryScope.php
+++ app/Scopes/LeaveDaysQueryScope.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Builder;
 
 trait LeaveDaysQueryScope
 {
-    public function funcLeaveDaysSearch(Builder $query, $search = null)
+    public function scopeFuncLeaveDaysSearch(Builder $query, $search = null)
     {
-        return $query->when($search != null, function ($q) use ($search) {
-            $q->where('id', 'like', '%' . $search . '%');
-        })->latest();
+        return $query->with('employee', 'leave', 'report')
+            ->when($search != null, function ($q) use ($search) {
+                $q->where('employees.name', 'like', '%' . $search . '%');
+            })->latest();
     }
 }
